41324554515a1db28ba196b5b39ce9549eb3c61d
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-12-14 09:38:59 +0200

    added model classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+simple = SimpleMDE()
 
 def create_app(config_name):
 
@@ -14,7 +26,13 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
-  
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+    #configure Uploadset
+    configure_uploadrs(app,photos)
 
     # Will add the views and forms
 
--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import os
 
 class Config:
+    SECRET_KEY = '12345'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://missy:yeet@localhost/test_blog'
 
 
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://missy:yeet@localhost/blog'
     DEBUG = True
 
 config_options = {
